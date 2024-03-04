b5d0a0d53ed545eb95d09c810e027495620f09b0
Michael Jones <mjj4@njit.edu>
2024-03-03 23:17:32 -0500

    Final submission

--- flask_react/src/App.js
+++ flask_react/src/App.js
@@ -4,22 +4,13 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 
-function Item(props) {
-  return <li>{props.message}</li>;
-}
-
-
 function App() {
 	// usestate for setting a javascript
 	// object for storing and using data
 	const [data, setdata] = useState({
       TopMovies : [],
       TopActors:  [],
-      MovieDetails: [],
-      ActorDetails: [],
-      ActorMovies: []
-  
-	});
+  	});
 	useEffect(() => {
 		fetch("/home").then((res) =>
 			res.json().then((data) => {
@@ -30,6 +21,47 @@ function App() {
 			})
 		);
 	}, []);
+
+  function Item(props) {
+    return <li onClick={() => Displaymoviedata(props.message)}>{props.message}</li>;
+  }
+
+  const [movie_data, setmovie_data] = useState({
+    MovieDetails : [],
+    TopActors:  [],
+  }); 
+
+  async function Displaymoviedata(string){
+    //const movie = { string };
+    
+    const response = await fetch("/ret_movie_details", {
+      method: "POST",
+      headers: {
+        'Content-Type' : 'application/json'
+      },
+      body: JSON.stringify(string)
+    })
+    if (response.ok){
+      console.log(response.json())
+      setmovie_data({
+        MovieDetails : response.data,
+        TopActors:  [],
+      });
+    } 
+  }
+
+/*  useEffect(() => {
+    fetch("/ret_movie_details").then((res) =>
+      res.json().then((movie_data) => {
+        setmovie_data({
+          MovieDetails: movie_data.MovieDetails,
+          TopActors: movie_data.TopActors
+        });
+      })
+    );
+    }, []);
+*/
+
 
 	return (
 		<div className="App">
@@ -43,6 +75,12 @@ function App() {
 				  <p>Top 5 Actors</p>
           <ul>
             {data.TopActors.map((message) => <Item key={message} message={message} />)}
+          </ul>
+        </div>
+        <div className="Movie-Data"> 
+          <p>Movie details</p>
+          <ul>
+            {movie_data.MovieDetails}
           </ul>
         </div>
 			</header>
